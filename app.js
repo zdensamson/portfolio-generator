8cb959d7d2360b9bea03db527f1e9948df93a7e9
Deku <zachdsamson@gmail.com>
2021-08-07 14:33:24 -0700

    develop merge

--- app.js
+++ app.js
@@ -9,9 +9,7 @@ const printProfileData = (profileDataArr) => {
   console.log('================');
 
   // Is the same as this...
-  profileDataArr.forEach((profileItem) => {
-    console.log(profileItem)
-  });
+  profileDataArr.forEach(profileItem => console.log(profileItem));
 };
 
 printProfileData(profileDataAgrs);
