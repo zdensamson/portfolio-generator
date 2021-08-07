a905dca9ee20f8f8f5f2dc7f76170dbee8164af2
Deku <zachdsamson@gmail.com>
2021-08-07 16:44:34 -0700

    dynamically generate html

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
-const profileDataAgrs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');const profileDataArgs = process.argv.slice(2);
+const generatePage = require('./src/page-template.js')
 
-const printProfileData = (profileDataArr) => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
+const [name, github] = profileDataArgs;
 
-  console.log('================');
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
 
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataAgrs);
-
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
