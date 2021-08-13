615938b3bda2d7fb64d8d4bc6f9be9ff19a0bc76
Deku <zachdsamson@gmail.com>
2021-08-13 15:28:11 -0700

    added getInventory & getStatus methods to player constructor function

--- app.js
+++ app.js
@@ -137,7 +137,7 @@ promptUser()
   .then(portfolioData => {
     const pageHTML = generatePage(portfolioData);
 
-    fs.writeFile('./index.html', pageHTML, err => {
+    fs.writeFile('./dist/index.html', pageHTML, err => {
       if (err) throw new Error(err);
 
       console.log('Page created! Check out index.html in this directory to see it!');
